fb9088581c17a9e791e6bf3a5c3b5a3840da7068
Gitahi-Moureen <moureengitahiwangari@gmail.com>
2024-03-29 11:42:17 +0300

    Added files

---
+++ functions.js
@@ -0,0 +1,27 @@
+// Given an array of strings, use a function to reverse all the elements in the string in ascending order and the specific elements in descending 
+
+
+let arr1 = ["Kenya","Tanzania","Uganda","Japan","India"]
+function arrsort(arr1){
+    arr1.sort();
+    return arr1;
+}
+console.log (arrsort(arr1));
+
+function reverse(arr1){
+    arr1.reverse(arr1);
+    return arr1;
+}
+console.log(reverse(arr1));
+
+//Using JS functions and an array of numbers, return positive if an element within the array is positive, negative if an element is negative, else zero
+
+let arr2 = [ 1,20,30,40,70]
+function countNumbers(arr2){
+    let pos_count =0;
+    let neg_count = 0;
+
+
+}
+
+//Write a function that accepts an array of numbers and uses the forEach() method to console.log each number multiplied by 2.
